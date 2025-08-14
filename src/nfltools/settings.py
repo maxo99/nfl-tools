b660fb5a5378c8776a784fcb82f811288d842a07
maxo <max.orr@mail.com>
2025-08-14 13:33:48 -0400

    feat: updating repo to incorporate fantasy draft repo as sub dependency

--- src/nfltools/settings.py
+++ src/nfltools/settings.py
@@ -1,9 +1,12 @@
 import os
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
+import git
+
+ROOT_DIR = str(git.Repo(".", search_parent_directories=True).working_tree_dir)
 DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+# Big Data Bowl dataset paths
 BDB_INPUT_DIR = Path("~/.kaggle/input").expanduser().resolve()
 BDB_2024_DIR = os.path.abspath(f"{BDB_INPUT_DIR}/nfl-big-data-bowl-2024")
 BDB_2025_DIR = os.path.abspath(f"{BDB_INPUT_DIR}/nfl-big-data-bowl-2025")
-
